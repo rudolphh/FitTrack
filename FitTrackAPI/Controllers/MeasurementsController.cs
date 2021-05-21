ef38f66b2cbfc19dcb88bc455b8e7aace841aac2
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-05-21 11:43:41 -0700

    add query parameters

--- FitTrackAPI/Controllers/MeasurementsController.cs
+++ FitTrackAPI/Controllers/MeasurementsController.cs
@@ -1,6 +1,8 @@
-﻿using FitTrackAPI.Models;
+﻿using FitTrackAPI.Classes;
+using FitTrackAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,16 +16,34 @@ namespace FitTrackAPI.Controllers
     {
         private readonly TrackContext _context;
 
-        public MeasurementsController(TrackContext context) {
+        public MeasurementsController(TrackContext context) 
+        {
             _context = context;
 
             _context.Database.EnsureCreated();
         }
 
         [HttpGet]
-        public IEnumerable<Measurement> GetMeasurements()
+        public async Task<IActionResult> GetMeasurements([FromQuery] QueryParameters queryParameters)
         {
-            return _context.Measurements.ToArray();
+            IQueryable<Measurement> measurements = _context.Measurements;
+
+            measurements = measurements
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+                
+            var PaginatedMeasurements = await measurements.ToArrayAsync();
+            return Ok(PaginatedMeasurements);
         }
+
+        [HttpGet, Route("/measurements/{id}")]
+        // can also use shortcut [HttpGet("{id:int}")] :int used for typing uri variable as well
+        public async Task<IActionResult> GetMeasurement(int id)
+        {
+            var measurement = await _context.Measurements.FindAsync(id);
+
+            return measurement == null ? NotFound() : Ok(measurement);
+        }
+
     }
 }
